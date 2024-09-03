306eb220d4df77b9bacad838bbb63e4f3075e1a3
Imtiyazoddin shaikh <116560639+divineDev-dotcom@users.noreply.github.com>
2024-09-03 13:03:35 +0530

    modifyed users model
    replaced Date.now() with Date.now, with some other changes.

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -4,41 +4,41 @@ Description:
 	Contains details of a user including personal details, profile picture, and role (student, faculty, admin).
 	Also, contains the information about when and by whom the document was created / updated.
 */
+
 const mongoose = require("mongoose");
-const {Schema, Types} = mongoose;
-const {ObjectId} = Types;
+const { Schema, ObjectId } = mongoose;
 
 const userSchema = new Schema({
-userName: {type: String, required: true, unique: true},
-password: {type: String, required: true},
-role: {
-type: String,
-enum: ["student", "faculty", "admin"],
-required: true
-},
-personalDetails: {
-firstName: {type: String, required: true},
-lastName: {type: String, required: true},
-email: {type: String, required: true, unique: true},
-phone: {type: String},
-address: {type: String},
-city: {type: String},
-country: {type: String},
-zipCode: {type: Number}
-},
-profilePicture: {type: String},
-createdAt: {type: Date, default: Date.now()},
-createdBy: {type: ObjectId},
-updatedAt: {type: Date, default: Date.now()},
-updatedBy: {type: ObjectId}
+  userName: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["student", "faculty", "admin"],
+    required: true
+  },
+  personalDetails: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String },
+    address: { type: String },
+    city: { type: String },
+    country: { type: String },
+    zipCode: { type: Number }
+  },
+  profilePicture: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  createdBy: { type: ObjectId },
+  updatedAt: { type: Date, default: Date.now },
+  updatedBy: { type: ObjectId }
 });
 
-// middleware to be executed before saving updatedAt field
+// Middleware to update updatedAt field before saving
 userSchema.pre("save", function (next) {
-this.updatedAt = Date.now();
-next();
+  this.updatedAt = Date.now();
+  next();
 });
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
