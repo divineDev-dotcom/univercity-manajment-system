c28e31ab393b8b33ee5fad3e5c98de5a867ced48
Jasdeep-GITf <kr.jasdeep@gmail.com>
2024-09-03 19:46:05 +0530

    Subject: refactor (user-model): "added timestamps and trims" Body: Added timestamps to the user model that will automatically create the createdAt and updatedAt fields. These fields can be accessed like any other field by using dot notation. Also, added "trim: true" to some fields so that leading or trailing white spaces are removed before inserting or updating.

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -1,7 +1,7 @@
 /*
 Model name: User
 Description: 
-	Contains details of a user including personal details, profile picture, and role (student, faculty, admin).
+	Contains details of a user including personal details, profile picture, and role like, student, faculty, admin, etc.
 	Also, contains the information about when and by whom the document was created / updated.
 */
 
@@ -9,35 +9,29 @@ const mongoose = require("mongoose");
 const { Schema, ObjectId } = mongoose;
 
 const userSchema = new Schema({
-  userName: { type: String, required: true, unique: true },
+  userName: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
   role: {
     type: String,
-    enum: ["student", "faculty", "admin"],
+    enum: ["student", "faculty", "admin", "super-admin"],
     required: true
   },
   personalDetails: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String },
-    address: { type: String },
-    city: { type: String },
-    country: { type: String },
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    phone: { type: String, trim: true  },
+    address: { type: String, trim: true },
+    city: { type: String, trim: true  },
+    country: { type: String, trim: true },
     zipCode: { type: Number }
   },
-  profilePicture: { type: String },
-  createdAt: { type: Date, default: Date.now },
-  createdBy: { type: ObjectId },
-  updatedAt: { type: Date, default: Date.now },
-  updatedBy: { type: ObjectId }
-});
-
-// Middleware to update updatedAt field before saving
-userSchema.pre("save", function (next) {
-  this.updatedAt = Date.now();
-  next();
-});
+  profilePicture: { type: String, trim: true },
+  createdBy: { type: ObjectId, ref: "User" },
+  updatedBy: { type: ObjectId, ref: "User" }
+}, 
+{timestamps: true}
+);
 
 const User = mongoose.model("User", userSchema);
 
