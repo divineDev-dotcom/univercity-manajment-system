43c7365aa7433b53305f4831d0d70faf3a1c1729
Jasdeep-GITf <kr.jasdeep@gmail.com>
2024-09-09 18:24:44 +0530

    modified user-controller.js and jwt.js according to the discussed response format

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const User = require("../models/user-model");
 
 const registerUser = async (req, res) => {
@@ -7,7 +8,7 @@ try {
 // ensure that the userName or email does not exist in the system
 const existingUser = await User.findOne({ $or: [{userName}, {email}] });
 if (existingUser) {
-return res.status(400).json({msg: "Username or email already exists"});
+return res.status(400).json({error: true, msg: "Username or email already exists"});
 }
 
 // else create a new user object
@@ -19,9 +20,9 @@ updatedBy: req.user._id
 
 // save the user to the database
 await newUser.save();
-res.status(201).json({msg: "User registered successfully", user: newUser});
+return res.status(201).json({error: false, msg: "User registered successfully", data: newUser});
 } catch(error) {
-return res.status(500).json({msg: "Error saving user", error: error.message});
+return res.status(500).json({error: true, msg: `Error saving user: ${error.message}`});
 }
 };
 
--- backend/middleware/jwt.js
+++ backend/middleware/jwt.js
@@ -16,14 +16,14 @@ if (authHeader && authHeader.startsWith("Bearer")) {
 const token = authHeader.split(" ")[1]; // splitting the value of authorization header to get its second part - the token
 jwt.verify(token, secretKey, (error, payload) => {
 if (error) {
-return res.status(403).json( {msg: "Invalid token"} );
+return res.status(403).json( {error: true, msg: "Invalid token"} );
 }
 // else received a valid JWT token which will be checked for authentication 
 req.user = payload; // assigning the decoded user credentials to be processed further
 next(); // calling the next middleware
 });
 } else {
-return res.status(401).json( {msg: "Token is missing", error: error.message} );
+return res.status(401).json( {error: true, msg: `Token is missing: ${error.message}`} );
 }
 };
 
