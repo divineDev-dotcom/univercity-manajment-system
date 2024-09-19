2ad441c42700888e8ee97ea667cf37394424de5e
Jasdeep-GITf <kr.jasdeep@gmail.com>
2024-09-19 20:34:56 +0530

    route access authentication function names changed
    Modified the names of route access

--- backend/routers/user-router.js
+++ backend/routers/user-router.js
@@ -11,7 +11,7 @@ Routes and their access:
 const express = require("express");
 const authenticateJWT = require("../middleware/jwt.js");
 const {isValidId} = require("../middleware/data-validations");
-const {checkAdmin, checkAdminOrSelf} = require("../middleware/route-access-authentication");
+const {isAdmin, isAdminOrSelf} = require("../middleware/route-access-authentication");
 const {login, registerUser, getProfileById, updateUserProfile} = require("../controllers/user-controller");
 const addUser = require("../controllers/user/add-user");
 
@@ -21,10 +21,10 @@ const userRouter = express.Router();
 userRouter.post("/login", login);
 
 // protected routes
-userRouter.post("/add", authenticateJWT, checkAdmin, addUser);
+userRouter.post("/add", authenticateJWT, isAdmin, addUser);
 userRouter.post("/register",  authenticateJWT, registerUser);
-userRouter.get("/profile", authenticateJWT, isValidId, checkAdminOrSelf, getProfileById);
-userRouter.put("/update-profile/:_id", authenticateJWT, isValidId, checkAdminOrSelf, updateUserProfile);
+userRouter.get("/profile", authenticateJWT, isValidId, isAdminOrSelf, getProfileById);
+userRouter.put("/update-profile/:_id", authenticateJWT, isValidId, isAdminOrSelf, updateUserProfile);
 
 userRouter.get("/test", (req, res) => {
 res.send("Testing user routes.");
