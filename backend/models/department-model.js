64284ffbbe9cf55a67d006cd87b2b24d0ea653ba
Azm1590 <ashishmungmode7@gmail.com>
2024-09-20 11:23:57 +0530

    edit department model
    remove unwanted code

--- backend/models/department-model.js
+++ backend/models/department-model.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const { Schema, ObjectId } = mongoose;
 
-const departmentSchema = new mongoose.Schema({
+const departmentSchema = new Schema({
     departmentCode: { type: String, required: true },
     departmentName: { type: String, required: true },
     headOfDepartment: { type: String},
 createdBy: {type: ObjectId, ref: 'User'},
 updatedBy: {type: ObjectId, ref: 'User'}
 }, 
-{timestamps: true});
+{timestamps: true}
+);
 
 const Department = mongoose.model('Department', departmentSchema);
 
